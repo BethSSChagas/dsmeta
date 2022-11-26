e28699924fda0039972f0d70d7c7d0962e451076
Bethss <bethsschagas@hotmail.com>
2022-11-26 11:07:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
-return(<h1> Olá Mundo!</h1>
+import NotificationButton from "./components/NotificationButton"
 
-)
+function App() {
+  return (
+    <>
+      <h1> Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
