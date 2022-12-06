706a7b79010ab745ead7b669e5503313d69eaeda
Bethss <bethsschagas@hotmail.com>
2022-12-06 19:27:01 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -11,25 +11,25 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long Id;
+	private Long id;
 	private String sellerName;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
-
+	
 	public Sale() {
 	}
 
 	public Long getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Long id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getSellerName() {
@@ -71,6 +71,6 @@ public class Sale {
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-
 	
-}
+	
+	}
