6468a0811b6c70de19c61e158ee41a7608134a1a
Bethss <bethsschagas@hotmail.com>
2022-12-10 09:56:39 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,15 +19,16 @@ function SalesCard() {
 
     useEffect(() => {
 
-        const dmin = minDate.toISOString();
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
 
         console.log(dmin);
 
-        axios.get(` ${BASE_URL}/sales `)
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
